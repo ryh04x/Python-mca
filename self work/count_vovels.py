45b8c1d087eec02eab7fa13f68075a8466598a4d
ryh04x <work4rhythm@gmail.com>
2024-09-02 16:28:02 +0530

    class questions solving

--- self work/count_vovels.py
+++ self work/count_vovels.py
@@ -10,7 +10,7 @@ def count_vowels(s):
     # set vovel count in string to zero
     count = 0
     
-#    loop to check each char in string
+    # loop to check each char in string
    
     for char in s:
         if char in vovels:
@@ -18,14 +18,15 @@ def count_vowels(s):
             
             
     return count
-        
-        
+                
+
 while True:
     # Take input from user
     input_string = input("Enter a string to check for vowels (or type 'exit' to quit): ")
     
     
     # Check if user wants to exit
+    
     if input_string.lower() == 'exit':
         print("Exiting the program.")
         break
